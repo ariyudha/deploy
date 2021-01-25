14db386413df0e19b23e3a4fb8f52c364ce2304f
ariyudha <66833224+ariyudha@users.noreply.github.com>
2021-01-26 06:59:12 +0700

    Delete resources directory

--- resources/views/layouts/js.blade.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript" src="{{ asset('azure/code/scripts/jquery.js') }}"></script>
-<script type="text/javascript" src="{{ asset('azure/code/scripts/bootstrap.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('azure/code/scripts/custom.js') }}"></script>
